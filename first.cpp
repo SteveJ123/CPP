0580baf3766817b0e0b8a998729ddb0d82ee2ffd
unknown <jjanardhan@gmail.com>
2019-04-29 23:04:17 +0530

    added

--- first.cpp
+++ first.cpp
@@ -1,27 +1,85 @@
 #include<iostream>
 using namespace std;
 
-// int a =5;
-// int b=5;
-// inline int add(){
 
-//     return (a + b);
+
+char &replacechar(int i);
+// class List {
+//     int next;
+//    List() : next(10) {}
+// }
+// why they initialize next like this
+// why not like this
+// class List {
+//     int next;
+//    List() { next = 10;  }
 // }
 
 
+class List {
+    
+    int id;
+    string name;
 
 
-inline int add(int a, int b){
-
-    return (a + b);
+public:
+List(int d, string n){
+    id = d;
+    name = n;
 }
 
+void display(){
+    cout<<"id:  "<<id<<endl;
+    cout<<"name:  "<<name<<endl;
+}
+};
+
+
+// char str[20]="hello World";
+string str ="hello World";
+
 int main(){
-int a = 10;
-int b = 10;
 
-cout<<"sum of numbers"<<add(a, b)<<endl;
-cout<<"this is test";
 
+// List l = List(1, "David");
+// List l2 =List(2, "Boon");
+
+// l.display();
+// l2.display();
+
+
+    // string s="some string";
+    // string &t = s;
+    // t[0]= "S";
+
+    // s[0]="S";
+    // cout<<s.begin()<<endl;
+    // if (t.begin() != t.end()) {        
+    //     auto ot = t.begin();
+    //     *ot = toupper(*ot);
+          
+    // }
+
+
+
+
+    // cout<<s<<endl;
+
+
+
+//     string str ("Test string");
+//   for (string::iterator it=str.begin(); it!=str.end(); ++it)
+//     std::cout << *it;
+//   std::cout << '\n';
+// replacechar(4);
+replacechar(0)='H';
+cout<<str<<endl;
     return 0;    
+}
+
+
+char &replacechar(int i){
+// str[i] = "X";
+// cout<<str[i]<<endl;
+    return str[i];
 }
--- readme.txt
+++ readme.txt
@@ -6,6 +6,13 @@ inline function
 //     return (a + b);
 // }
 
+
+
+
+if you try to access a and b inside main by making int a, b in gloabl scope
+then compiler will initialize default value to 0.
+It is called default parameter.
+
 int main(){
 int a = 10;
 int b = 10;
@@ -31,5 +38,35 @@ int b = 10;
 cout<<"sum of numbers"<<add(a, b)<<endl; //output 20
 cout<<"this is test";
 
+    return 0;    
+}
+//value inside main is used
+
+/**/
+
+
+#include<iostream>
+using namespace std;
+
+class List {
+    
+    int next;
+
+public:
+void setnext(int x){
+    next = x;
+}
+
+int getnext(){
+    return next;
+}
+};
+
+
+int main(){
+
+List l;
+l.setnext(10);
+cout<<"next value is: "<<l.getnext();
     return 0;    
 }
